cf86a1d1e30c3092b44f9a0f34a28a26f107688c
Liam Clink <clink.6@osu.edu>
2022-08-28 15:50:44 -0400

    Started generalizing read_from_file

--- src/io.rs
+++ src/io.rs
@@ -11,7 +11,6 @@ where
     I: ExactSizeIterator,
     <I as Iterator>::Item: ToString,
 {
-    let _create_dir_success = std::fs::create_dir("test_data");
     let file = std::fs::OpenOptions::new()
         .write(true)
         .create(true)
@@ -35,9 +34,11 @@ where
 
 // Use assertions to check for problems
 #[test]
-fn test_write() -> Result<(), Box<dyn Error>> {
-    let test: Vec<f64> = vec![1.423, 0.61324, 123.865];
-    write_to_file(test)?;
+fn test_write() -> Result<(), Box<dyn Error>>
+{
+    let test_vec: Vec<f64> = vec![1.423, 0.61324, 123.865];
+    let _remove_success = std::fs::remove_file("test_data/data.tsv");
+    write_to_file(test_vec)?;
     // Could use assert_eq! and open file and check matching
     let _remove_success = std::fs::remove_file("test_data/data.tsv");
     Ok(())
@@ -45,9 +46,39 @@ fn test_write() -> Result<(), Box<dyn Error>> {
 
 // Read from a file
 use std::io::Read;
-pub fn read_from_file() {
-    let mut file = std::fs::File::open("data.txt").unwrap();
+pub fn read_from_file<A, B>(fpath: &str) -> Result<A, B::Err>
+where
+    A: FromIterator<B>,
+    B: std::str::FromStr,
+{
+    let mut file = std::fs::File::open(fpath).unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
-    print!("{}", contents);
+
+    let result = contents
+        .split('\t')
+        .map(|x| {
+            let element = x.parse::<B>();
+            match element
+            {
+                Ok(valid_element) =>
+                {
+                    return valid_element;
+                }
+                Err(err) =>
+                {
+                    eprintln!("Element in input file is not valid: {err}");
+                }
+            }
+        })
+        .collect();
+    result
+}
+
+#[test]
+fn test_read() -> Result<(), Box<dyn Error>>
+{
+    let test_vec: Vec<f64> = read_from_file("test_data/data.tsv");
+
+    Ok(())
 }
