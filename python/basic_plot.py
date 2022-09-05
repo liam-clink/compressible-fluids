Fixed laplacian calculation

python/basic_plot.py
@@ -2,12 +2,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-file = open("test_data/data.tsv")
 
-data = []
-for i in range(15):
-    data.append(np.fromstring(file.readline(), sep="\t"))
+data = np.loadtxt("test_data/data.tsv")
 
-plt.plot(data[0])
-plt.plot(data[1])
+plt.plot(data)
 plt.show()