b533f1413a631f09485dfc182db00b645cae4dfc
ominusliticus <elkevin111@gmail.com>
2023-02-01 10:05:37 -0500

    Include vectorization

--- src/physics.rs
+++ src/physics.rs
@@ -1,8 +1,11 @@
-use num_traits::{Num, One, Zero};
+use num_traits::{Float, Zero};
 use std::fmt::Debug;
 
+#[allow(unused_imports)]
+use rayon::prelude::*;
+
 // This allows us to consolidate a lot of repetitve contraints needed below
-pub trait Field: Debug + Clone + Copy + Zero + One + Num {}
+pub trait Field: Debug + Clone + Copy + Sync + Send + Float {}
 
 // Replace float with scalar (could be complex or whatever else counts as a scalar
 // Probably should be a trait?)
@@ -24,15 +27,29 @@ pub struct Vector<const SIZE: usize, T: Field> {
     raw_vector: [T; SIZE], // What about functions as vectors?
 }
 
+impl<const SIZE: usize, T: Field> Vector<SIZE, T> {
+    pub fn data(&self) -> [T; SIZE] {
+        self.raw_vector.clone()
+    }
+}
+
+// Needed to call into_par_iter on Vector<T, SIZE> directly, and raw_vector can be left private
+impl<const SIZE: usize, T: Field> IntoParallelIterator for Vector<SIZE, T> {
+    type Iter = rayon::array::IntoIter<T, SIZE>;
+    type Item = T;
+
+    fn into_par_iter(self) -> Self::Iter {
+        self.raw_vector.into_par_iter()
+    }
+}
+
 // Scalar multiplication
 impl<const SIZE: usize, T: Field> std::ops::Mul<Vector<SIZE, T>> for Scalar<T> {
     type Output = Vector<SIZE, T>;
     fn mul(self, vector: Vector<SIZE, T>) -> Self::Output {
         let result = vector
-            .raw_vector
-            .to_vec()
-            .iter()
-            .map(|v| *v * self.value)
+            .into_par_iter()
+            .map(|v| v * self.value)
             .collect::<Vec<T>>();
         Vector {
             raw_vector: result
@@ -42,21 +59,54 @@ impl<const SIZE: usize, T: Field> std::ops::Mul<Vector<SIZE, T>> for Scalar<T> {
     }
 }
 
-// Scalar multiplication again, but such that order doesn't matter
+// Scalar multiplication again, but reverse argument order
 impl<const SIZE: usize, T: Field> std::ops::Mul<Scalar<T>> for Vector<SIZE, T> {
     type Output = Vector<SIZE, T>;
     fn mul(self, scalar: Scalar<T>) -> Self {
         let result = self
-            .raw_vector
-            .to_vec()
-            .iter()
-            .map(|v| *v * scalar.value)
+            .into_par_iter()
+            .map(|v| v * scalar.value)
             .collect::<Vec<T>>();
         Vector {
             raw_vector: result
                 .try_into()
                 .unwrap_or_else(|v: Vec<T>| panic!("Could not convert {:?} to array", v)),
         }
+    }
+}
+
+// Vector addition
+impl<const SIZE: usize, T: Field> std::ops::Add for Vector<SIZE, T> {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        let result = self
+            .into_par_iter()
+            .zip(other.into_par_iter())
+            .map(|(a, b)| a + b)
+            .collect::<Vec<T>>();
+        Vector {
+            raw_vector: result
+                .try_into()
+                .unwrap_or_else(|v: Vec<T>| panic!("Could not convert {:?} to array", v)),
+        }
+    }
+}
+
+impl<const SIZE: usize, T: Field + std::ops::AddAssign> InnerProductSpace for Vector<SIZE, T> {
+    type TField = T;
+
+    fn dot(&self, other: &Self) -> T {
+        let mut result: T = T::zero();
+        for value in self
+            .clone()
+            .into_par_iter()
+            .zip(other.clone().into_par_iter())
+            .map(|(a, b)| a * b)
+            .collect::<Vec<T>>()
+        {
+            result += value;
+        }
+        result
     }
 }
 
@@ -65,19 +115,55 @@ impl<const SIZE: usize, T: Field> std::ops::Mul<Scalar<T>> for Vector<SIZE, T> {
 // Vector fields
 
 // Functions as vectors (composition of functions)
-fn quadratic<T: Field>(x: Scalar<T>) -> T {
-    x.value * x.value
+// fn quadratic<T: Field>(x: Scalar<T>) -> T {
+//     x.value * x.value
+// }
+//
+// fn cubic<T: Field>(x: Scalar<T>) -> T {
+//     x.value * x.value * x.value
+// }
+
+/************************************************
+           Traits for Vector Spaces
+*************************************************/
+
+pub trait InnerProductSpace {
+    type TField: Field;
+
+    fn dot(&self, other: &Self) -> Self::TField;
+
+    fn norm2(&self) -> Self::TField {
+        Self::dot(self, self)
+    }
+
+    fn linearly_independent(&self, other: &Self) -> bool {
+        Self::dot(self, other) == Self::TField::zero()
+    }
 }
 
-fn cubic<T: Field>(x: Scalar<T>) -> T {
-    x.value * x.value * x.value
-}
-
-pub trait InnerProductVector<T: Field, Rhs = Self> {
-    fn dot(&self, rhs: &Rhs) -> T;
-    fn norm(&self) -> T;
-}
-
-pub trait CrossableVector<Rhs = Self> {
+pub trait OuterProductVector<Rhs> {
     fn cross(&self, rhs: &Rhs) -> Self;
+}
+
+// Tests
+#[cfg(test)]
+mod tests {
+    use num_traits::Float;
+
+    #[test]
+    fn test_scalar_multiplication() {
+        let s = crate::physics::Scalar {
+            value: (10.0 as dyn Float),
+        };
+        let v = crate::physics::Vector {
+            raw_vector: [(1.0 as dyn Float); 10],
+        };
+        assert_eq!(v.data(), [10.0; 10]);
+    }
+
+    // #[test]
+    // fn test_vector_addition() ({}
+    //
+    // #[test]
+    // fn test_inner_product_space() {}
 }
